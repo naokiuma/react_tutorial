5ce01cdb64bc72ddac76a1c518a9e2a18491b1f0
nao mac <sagalumaata@gmail.com>
2021-08-23 11:39:16 +0900

    story_book追加

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { Button } from './Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const PrimaryBig = Template.bind({});
+PrimaryBig.args = {
+  primary: true,
+  label: 'Button',
+  size: 'large',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
