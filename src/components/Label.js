0163484bbc50158a505195433507574d3e4bc0a9
kim Nao <sagalumaata@gmail.com>
2021-09-02 20:53:02 +0900

    label追加

--- src/components/Label.js
+++ src/components/Label.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
-export default function Label({ label:{title,state } }){
+export default function Label({ label:{title,state,type } }){
+    if(type=="check"){
+        return(
+            <div class="tc">
+                <p className="description tc">ボタンタイプ：{type}</p>
+                <label class="check-box" for="check01">
+                    <input type="checkbox" id="check01" />
+                    <span class="text">たけのこの里</span>
+                </label>
+            </div>
+        );
+
+    }
+
+    if(type="span"){
+        return(
+            <div class="tc">
+                <p className="description tc">ボタンタイプ：{type}</p>
+                <span class="required_label">必須</span>
+            </div>
+        )
+    }
+
     return(
         
-        <div className="test">
-        <a className={`${state}`}>{title}</a>
+        <div className="labels">
+            <a className={`${state}`}>{title}</a>
         </div>
+
+        
 
 
     );
--- src/components/Label.stories.js
+++ src/components/Label.stories.js
@@ -12,7 +12,28 @@ export const Default = Template.bind({});
 Default.args = {
     label:{
         title:'カテゴリー',
+        type:'label',
         state:'label01',
+        updatedAt:new Date(),
+    },
+};
+
+export const CheckBox = Template.bind({});
+CheckBox.args = {
+    label:{
+        title:'チェックボックス',
+        type:'check',
+        state:'label02',
+        updatedAt:new Date(),
+    },
+};
+
+export const Required1 = Template.bind({});
+Required1.args = {
+    label:{
+        title:'必須ラベル',
+        type:'span',
+        state:'label03',
         updatedAt:new Date(),
     },
 };
