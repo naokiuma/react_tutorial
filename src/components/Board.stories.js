2e7451003bb04a3cecc9e08c9cddd7a0bdb09550
nao mac <sagalumaata@gmail.com>
2021-10-06 01:19:52 +0900

    board追加

--- src/components/Board.stories.js
+++ src/components/Board.stories.js
@@ -9,21 +9,29 @@ export default {
     component:Board,
     title:'Board',
     
-    decorators:[story => <div style= {{ padding:'3rem'}}> {story()}</div>],
+    // decorators:[story => <div style= {{ padding:'3rem'}}> {story()}</div>],
 }
 
 const Template = args => <Board {...args} />;
 export const Default = Template.bind({});
 
-Default.args = {
-    title:'maru',
-    marus:[
-        { ...SquareStories.Maru.args.square, id: '1', title: 'maru 1' },
-        { ...SquareStories.Maru.args.square, id: '2', title: 'maru 2' },
-        { ...SquareStories.Maru.args.square, id: '3', title: 'maru 3' },
-        { ...SquareStories.Maru.args.square, id: '4', title: 'maru 4' },
-        { ...SquareStories.Maru.args.square, id: '5', title: 'maru 5' },
-        { ...SquareStories.Maru.args.square, id: '6', title: 'maru 6' },
-    ],
+// Default.args = {
+//     squares:{
+//         title:'maru',
+//         marus:[
+//             { ...SquareStories.Default.args.square, id: '1', title: 'maru 1' },
+//             { ...SquareStories.Default.args.square, id: '2', title: 'maru 2' },
+//             { ...SquareStories.Default.args.square, id: '3', title: 'maru 3' },
+//             { ...SquareStories.Default.args.square, id: '4', title: 'maru 4' },
+//             { ...SquareStories.Default.args.square, id: '5', title: 'maru 5' },
+//             { ...SquareStories.Default.args.square, id: '6', title: 'maru 6' },
+//         ],
+//     },
+//     onclick:() =>{}
+// };
 
+//dekita
+Default.args = {
+    squares:['◯','◯','◯','◯','◯','◯','◯','◯','◯'],
+    onClick: () => {},
 };
--- src/components/ticktacktoo/Board.js
+++ src/components/ticktacktoo/Board.js
@@ -2,40 +2,77 @@ import React from 'react';
 import Square from './Square';
 
 
-export class Board extends React.Component {
-    
-    renderSquare(i) {
+export default function Board({squares,onClick}){
+    console.log("board開始")
+    console.log(squares)
+    const renderSquare = (i) =>{
         return <Square 
-                
-        value={this.props.squares[i]}
-                //React では、イベントを表す props には on[Event] という名前、イベントを処理するメソッドには handle[Event] という名前を付けるのが慣習となっています。
-                onClick={() => this.props.onClick(i)}
-                />;
+        value={squares[i]}
+            //React では、イベントを表す props には on[Event] という名前、
+            //イベントを処理するメソッドには handle[Event] という名前を付けるのが慣習となっています。
+            onClick={() => onClick(i)}
+            />;
     }
-    
-    
-    render() {
-        // if(this.title == "maru"){
-        // }
-
-        return (
-        <div>
-            <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            </div>
+        
+    return (
+    <div>
+        <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
         </div>
-        );
-    }
-   }
+        <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+        </div>
+        <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+        </div>
+    </div>
+    );
+    
+   
+
+
+
+}
+// export default class Board extends React.Component {
+    
+//     renderSquare(i) {
+//         return <Square 
+                
+//         value={this.props.squares[i]}
+//                 //React では、イベントを表す props には on[Event] という名前、イベントを処理するメソッドには handle[Event] という名前を付けるのが慣習となっています。
+//                 onClick={() => this.props.onClick(i)}
+//                 />;
+//     }
+    
+    
+//     render() {
+//         // if(this.title == "maru"){
+//         // }
+
+//         return (
+//         <div>
+//             <div className="board-row">
+//             {this.renderSquare(0)}
+//             {this.renderSquare(1)}
+//             {this.renderSquare(2)}
+//             </div>
+//             <div className="board-row">
+//             {this.renderSquare(3)}
+//             {this.renderSquare(4)}
+//             {this.renderSquare(5)}
+//             </div>
+//             <div className="board-row">
+//             {this.renderSquare(6)}
+//             {this.renderSquare(7)}
+//             {this.renderSquare(8)}
+//             </div>
+//         </div>
+//         );
+//     }
+//    }
